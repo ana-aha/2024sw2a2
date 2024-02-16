53ef1a2bcf1ffc749dde0f1401edd7ac40b9adbd
ana-aha laura <aninha.20.07.oli@gmail.com>
2024-02-16 12:26:23 +0000

    salvar1602

---
+++ app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
@@ -0,0 +1,13 @@
+package primeiro_exemplo.modelos;
+
+public class Pessoa {
+    // Atributos
+    public String nome;
+    public int idade;
+
+    //Métodos
+    public boolean verificarMaioridade() {
+        return this.idade >= 18;
+    }
+    
+}
